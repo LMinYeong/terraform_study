ac00873b2e1f2ed80a31eb0bcc125a83dbf3df79
LMinYeong <leen3357@insideinfo.co.kr>
2023-11-14 17:38:22 +0900

    vpc create test

--- main.tf
+++ main.tf
@@ -2,3 +2,13 @@ provider "aws" {
   region = "ap-southeast-1"
   # region = var.aws_region
 }
+
+resource "aws_vpc" "main" {
+  cidr_block       = "172.164.12.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "test"
+  }
+
+}
