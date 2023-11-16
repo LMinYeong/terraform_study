7edcb8e92e0ce127f684f238cb8ecb2f81fed9d4
LMinYeong <leen3357@insideinfo.co.kr>
2023-11-16 16:09:38 +0900

    new file:   aws-instance-test/.gitignore modified:   aws-instance-test/vpc.tf

--- aws-instance-test/vpc.tf
+++ aws-instance-test/vpc.tf
@@ -1,8 +1,8 @@
 # (1) Internet VPC
 resource "aws_vpc" "lmy-terraform-test" {
-  cidr_block = "10.0.0.0/16"
-  instance_tenancy = "default"
-  enable_dns_support = "true"
+  cidr_block           = "10.0.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_support   = "true"
   enable_dns_hostnames = "true"
   tags = {
     Name = "lmy-terraform-test"
@@ -11,21 +11,21 @@ resource "aws_vpc" "lmy-terraform-test" {
 
 # (2) Subnets
 resource "aws_subnet" "lmy-tf-public-1" {
-  vpc_id = aws_vpc.lmy-terraform-test.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id                  = aws_vpc.lmy-terraform-test.id
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone = "ap-southeast-1a"
+  availability_zone       = "ap-southeast-1a"
 
-    tags = {
-      Name = "lmy-tf-public-1"
-    }
+  tags = {
+    Name = "lmy-tf-public-1"
+  }
 }
 
 resource "aws_subnet" "lmy-tf-private-1" {
-  vpc_id = aws_vpc.lmy-terraform-test.id
-  cidr_block = "10.1.1.0/24"
+  vpc_id                  = aws_vpc.lmy-terraform-test.id
+  cidr_block              = "10.1.1.0/24"
   map_public_ip_on_launch = "false"
-  availability_zone = "ap-southeast-1a"
+  availability_zone       = "ap-southeast-1a"
 
   tags = {
     Name = "lmy-tf-private-1"
@@ -35,24 +35,25 @@ resource "aws_subnet" "lmy-tf-private-1" {
 # (3) Internet GW
 resource "aws_internet_gateway" "lmy-tf-gw" {
   vpc_id = aws_vpc.lmy-terraform-test.id
-  
+
   tags = {
     Name = "lmy-tf"
   }
+
 }
 
 # (4) Route Tables
 resource "aws_route_table" "lmy-tf-public" {
   vpc_id = aws_vpc.lmy-terraform-test.id
 
-  route = {
+  route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.lmy-tf-gw.id 
+    gateway_id = aws_internet_gateway.lmy-tf-gw.id
   }
-  
- tags = {
-   Name = "lmy-tf-public-1"
- }
+
+  tags = {
+    Name = "lmy-tf-public-1"
+  }
 
 }
 
@@ -62,11 +63,11 @@ resource "aws_route_table" "lmy-tf-private" {
 
 # (5) Route associations public
 resource "aws_route_table_association" "lmy-tf-public-1-rta" {
-  subnet_id = aws_subnet.lmy-tf-public-1.id
-  route_table_id = aws_route_table.lmy-tf-public
+  subnet_id      = aws_subnet.lmy-tf-public-1.id
+  route_table_id = aws_route_table.lmy-tf-public.id
 }
 
 resource "aws_route_table_association" "lmy-tf-private-1-rta" {
-  subnet_id = aws_subnet.lmy-tf-public-1.id
-  route_table_id = aws_route_table.lmy-tf-public
+  subnet_id      = aws_subnet.lmy-tf-public-1.id
+  route_table_id = aws_route_table.lmy-tf-public.id
 }
