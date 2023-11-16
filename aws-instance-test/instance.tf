ec579f13bc9bb5a22fac791cd34b50a138ec66f7
LMinYeong <leen3357@insideinfo.co.kr>
2023-11-16 11:35:42 +0900

    sg id gogo

--- aws-instance-test/instance.tf
+++ aws-instance-test/instance.tf
@@ -9,6 +9,7 @@ resource "aws_instance" "example" {
         Service = var.test
     }
 
-    security_groups = [ aws_security_group.from_singapore.id ]
+    # security_groups = [ aws_security_group.from_singapore.id ]
+    security_groups = [ "sg-0dc02689764a5db4e" ]
 
 }
