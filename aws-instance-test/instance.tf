5946c64e8933ab0b581c02e95e16ede1aadc4a84
LMinYeong <leen3357@insideinfo.co.kr>
2023-11-16 10:25:19 +0900

    modified:   aws-instance-test/instance.tf

--- aws-instance-test/instance.tf
+++ aws-instance-test/instance.tf
@@ -9,17 +9,25 @@ resource "aws_instance" "example" {
         Service = var.test
     }
 
-    connection {
-        type     = "ssh"
-        user     = "ec2-user"
-        password = var.root_password
-        host     = self.public_ip
+    #connection {
+    #    type     = "ssh"
+    #    user     = "ec2-user"
+    #    password = var.root_password
+    #    host     = self.public_ip
+    #}
+
+    #provisioner "remote-exec" {
+    #    inline = [
+    #        "echo ${self.private_ip} >> private_ips.txt"
+    #    ]
+    #}
+    provisioner "local-exec" {
+        command = "echo ${self.private_ip} >> private_ips.txt"
     }
 
-    provisioner "remote-exec" {
-        inline = [
-            "echo ${self.private_ip} >> private_ips.txt"
-        ]
+    provisioner "file" {
+      source = "./private_ips.txt"
+      destination = "/home/ec2-user/private_ips.txt"
     }
     
 }
