f2724b4bfcc9f5028ee08fe1fe1c6c7acb371864
LMinYeong <leen3357@insideinfo.co.kr>
2023-11-16 14:05:00 +0900

    new file:   aws-instance-test/default_vpc.tf modified:   aws-instance-test/securitygroup.tf

--- aws-instance-test/securitygroup.tf
+++ aws-instance-test/securitygroup.tf
@@ -1,20 +1,30 @@
 data "aws_ip_ranges" "singapore_ec2" {
-  regions = ["ap-southeast-1"]
-  services = ["ec2"]
+    regions = ["ap-southeast-1"]
+    services = ["ec2"]
 }
 
 resource "aws_security_group" "from_singapore" {
-  name = "from_singapore"
+    name = "from_singapore"
 
-  ingress {
-    from_port = "22"
-    to_port = "22"
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-  tags = {
-    CreateDate = data.aws_ip_ranges.singapore_ec2.create_date
-    SyncToken = data.aws_ip_ranges.singapore_ec2.sync_token
-  }
+    ingress {
+        from_port = "22"
+        to_port = "22"
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        
+    }
+
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+    
+    tags = {
+        Name = "from_singapore"
+        CreateDate = data.aws_ip_ranges.singapore_ec2.create_date
+        SyncToken = data.aws_ip_ranges.singapore_ec2.sync_token
+    }
 }
